11dc7fc50830dbf77de8b42104e23f878b300670
Gabriel Borges <gabriel.moura2009@hotmail.com>
2021-08-20 21:05:59 -0300

    simplificando a transformação de objetos e implementando sub recursos

--- algalog-api/src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -2,8 +2,9 @@ package com.algaworks.algalog.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -11,11 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import javax.persistence.OneToMany;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -27,32 +24,37 @@ import lombok.Setter;
 @Entity
 public class Entrega {
 
-	@Valid 
-	//@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull
 	@ManyToOne
 	private Cliente cliente;
 	
-	@Valid
-	@NotNull
-	@Embedded
 	private Destinatario destinatario;
 	
-	@NotNull
 	private BigDecimal taxa;
 	
-	@JsonProperty(access = Access.READ_ONLY)
+	@OneToMany(mappedBy = "entrega")
+	private List<Ocorrencia> ocorrencias = new ArrayList<>();
+	
 	@Enumerated(EnumType.STRING)
 	private StatusEntrega status;
 	
-	@JsonProperty(access = Access.READ_ONLY)
 	private OffsetDateTime dataPedido;
 	
-	@JsonProperty(access = Access.READ_ONLY)
 	private OffsetDateTime dataFinalizacao;
+
+	public Ocorrencia adicionarOcorrencia(String descricao) {
+		Ocorrencia ocorrencia = new Ocorrencia();
+		ocorrencia.setDescricao(descricao);
+		ocorrencia.setDataRegistro(OffsetDateTime.now());
+		ocorrencia.setEntrega(this);
+		
+		this.getOcorrencias().add(ocorrencia);
+		
+		return ocorrencia;
+		
+	}
 }
